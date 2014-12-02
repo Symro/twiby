0a309f1d364e185f8de8b007542ff97255f6697c
symro <sylvain.mro@gmail.com>
2014-12-02 22:49:08 +0100

    adding Tweet model {#3}

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202213105) do
+ActiveRecord::Schema.define(version: 20141202214422) do
+
+  create_table "tweets", force: true do |t|
+    t.integer  "user_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
