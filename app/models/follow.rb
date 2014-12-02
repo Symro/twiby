06eb2bb21b65c116ede31c1037947b6e34311865
symro <sylvain.mro@gmail.com>
2014-12-02 23:47:21 +0100

    adding models validation + model relations {#4}

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,2 +1,9 @@
 class Follow < ActiveRecord::Base
+
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
+  validates :followed_id, presence: true
+  validates :follower_id, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true, length: {maximum: 30}, uniqueness: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
+
+  has_many :tweets
+  has_many :favorites
+
 end
