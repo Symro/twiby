88461823e632ebdc07507cec19bbc64ba084e1ac
symro <sylvain.mro@gmail.com>
2014-12-06 14:11:48 +0100

    Users can sign up with a form chossing their password, mail and username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Before action for adding custom field to formulars
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+
+  protected
+  def configure_permitted_parameters
+    # Adding sanitizer for the input username to the sign up form
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    # devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
+  end
 end
