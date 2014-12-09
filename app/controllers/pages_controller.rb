4e02543c36af043e9cef9863a12261807449b12f
Symro <sylvain.mro@gmail.com>
2014-12-09 20:01:41 +0100

    check if the user is following the profile page to show the good action {#8}

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -15,6 +15,11 @@ class PagesController < ApplicationController
 
     @follow     = Follow.new
 
+    # Check if the current user follow this profile
+    if Follow.where(follower_id: current_user.id, followed_id: params[:id]).present?
+      @following = true;
+    end
+
     rescue ActiveRecord::RecordNotFound
       flash[:notice] = "This profile doesn't exist"
       redirect_to :action => 'index'
