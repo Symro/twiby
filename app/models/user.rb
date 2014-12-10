b25ec71f4839737d6b6839c281c68cca94040bef
Symro <sylvain.mro@gmail.com>
2014-12-10 14:35:06 +0100

    Finishing relations with followers and following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,7 @@ class User < ActiveRecord::Base
   has_many :active_follows, class_name: 'Follow', foreign_key: 'follower_id', dependent: :destroy
   has_many :following, through: :active_follows, source: :followed
 
+  has_many :passive_follows, class_name: 'Follow', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :passive_follows, source: :follower
+
 end
