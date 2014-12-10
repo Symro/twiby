79a199b86d608bde1d9de820f4e23ce69ced7d98
Symro <sylvain.mro@gmail.com>
2014-12-10 23:11:02 +0100

    Adding ajax profile edition

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -4,13 +4,29 @@
 $(document).ready(function(){
 
 
-	$('.yell_something').on('click',function(){
-		$(this).fadeOut();
-		setTimeout(function(){
-			$('.yell_form').fadeIn();
-		},500);
-	});
+    $('.yell_something').on('click',function(){
+        $(this).fadeOut();
+        setTimeout(function(){
+            $('.yell_form').fadeIn();
+        },500);
+    });
 
+    // edit form
+    $('.edit_user').on('submit', function(e){
+        e.preventDefault();
+        var data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: '/users',
+            data: data
+        })
+        .done(function(){
+            console.log('done');
+        })
+        .fail(function(){
+            console.log('error');
+        })
+    });
 
 });
 
