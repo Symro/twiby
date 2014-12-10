38b049b372f2e4d4789de6511a4a6052fb18c6a0
Symro <sylvain.mro@gmail.com>
2014-12-10 13:34:09 +0100

    Decomment user validation in model User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  #validates :username, presence: true, length: {maximum: 30}, uniqueness: true
+  validates :username, presence: true, length: {maximum: 30}, uniqueness: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, uniqueness: true
 
   has_many :tweets
