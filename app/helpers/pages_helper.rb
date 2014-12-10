972e50a5e84de39461af33cb293357a949599649
Symro <sylvain.mro@gmail.com>
2014-12-10 17:28:24 +0100

    Trying to add edit form in profile page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+  def resource_name
+    current_user.username || :user
+  end
+
+  def resource
+    # @resource ||= User.new
+    @resource ||= current_user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
