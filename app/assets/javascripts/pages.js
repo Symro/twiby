3ba00ef2f5447793a3b593fd8410b6bad7768576
Alex <alexandre.guerard6@gmail.com>
2014-12-11 03:18:33 +0100

    Inté modal + scroll top + new logo

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -11,6 +11,51 @@ $(document).ready(function(){
 		},500);
 	});
 
+	
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.yell_it_btn').fadeIn();
+		} else {
+			$('.yell_it_btn').fadeOut();
+		}
+	});
+	
+	$('.yell_it_btn').click(function(){
+		$('html, body').animate({scrollTop : 0},800);
+		$('.yell_something').fadeOut();
+		setTimeout(function(){
+			$('.yell_form').fadeIn();
+		},800);
+
+		return false;
+	});
+
+	$('.modal_follower').on('click',function(e){
+		e.preventDefault();
+		$('.modal_box').fadeIn();
+		setTimeout(function(){
+			$('.modal_follower').fadeIn().addClass('modal_window_visible');
+		},300);
+	});
+
+	$('.modal_following').on('click',function(e){
+		e.preventDefault();
+		$('.modal_box').fadeIn();
+		setTimeout(function(){
+			$('.modal_following').fadeIn().addClass('modal_window_visible');
+		},300);
+	});
+
+	$('.close_modal').on('click',function(e){
+		e.preventDefault();
+		$('.modal_following').removeClass('modal_window_visible');
+		$('.modal_follower').removeClass('modal_window_visible');
+
+		setTimeout(function(){
+			$('.modal_box').fadeOut();
+		},300);
+	});
+
 
 });
 
