a6ff7c5770624a496df769eb59b8e79bacb6c73f
Symro <sylvain.mro@gmail.com>
2014-12-13 17:28:49 +0100

    Refresh after update

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -75,6 +75,7 @@ $(document).ready(function(){
         })
         .done(function(){
             console.log('done');
+            location.reload();
         })
         .fail(function(){
             console.log('error');
